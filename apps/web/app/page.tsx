55c08681914b8787de2363862b8fdebd2689e1ae
unknown <isaac.noriega@iteso.mx>
2026-02-01 18:11:28 -0600

    refactor(web): move audience switching to app/page.tsx; mark page dynamic and await searchParams

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,40 @@
 import Navbar from "@/components/layout/Navbar";
-import HeroSwitcher from "@/components/home/HeroSwitcher";
-import CategoriesSection from "@/components/home/CategoriesSection";
-import CatalogSection from "@/components/home/CatalogSection";
+import HeroParentsSection from "@/components/home/HeroParentsSection";
+import HeroInstitutionSection from "@/components/home/HeroInstitutionSection";
+import CategoriesSection from "@/components/home/CategoriesParentsSection";
+import CatalogSection from "@/components/home/CatalogParentsSection";
+import CallParentsSections from "@/components/home/CallParentsSections";
+import ChallengeInstitutionsSection from "@/components/home/ChallengeInstitutionsSection";
+import BussinessSections from "@/components/home/BussinessSections";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+type SearchParams = { [key: string]: string | string[] | undefined };
+
+export default async function Home({ searchParams }: { searchParams?: Promise<SearchParams> }) {
+	const params = (await searchParams) ?? {};
+	const rawAudience = params.audience;
+	const audience = Array.isArray(rawAudience) ? rawAudience[0] : rawAudience;
+	const isParents = audience !== "schools"; // default to parents when undefined
 	return (
 		<>
 			<Navbar />
 			<main>
-				<HeroSwitcher />
-				<CategoriesSection />
-				<CatalogSection />
+				{isParents ? (
+					<>
+						<HeroParentsSection />
+						<CategoriesSection />
+						<CatalogSection />
+						<CallParentsSections />
+					</>
+				) : (
+					<>
+						<HeroInstitutionSection />
+						<ChallengeInstitutionsSection />
+						<BussinessSections />
+					</>
+				)}
 			</main>
 		</>
 	);
