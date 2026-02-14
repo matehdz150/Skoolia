a206e6b0d71c249bde494e9cef91e652a9ca446b
Mateo Hernández Gutiérrez <matehdz150@gmail.com>
2026-02-14 15:55:36 -0600

    feat[web]: servicios en front para llamadas api de auth

---
+++ apps/web/lib/services/auth.service.ts
@@ -0,0 +1,49 @@
+// src/services/auth.service.ts
+
+import { api } from "./api";
+
+export type UserRole = 'public' | 'private';
+
+export interface RegisterInput {
+  email: string;
+  password: string;
+  role: UserRole;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export interface AuthMessageResponse {
+  message?: string;
+  success?: boolean;
+}
+
+export const authService = {
+  async register(data: RegisterInput) {
+    return api<AuthMessageResponse>('/auth/register', {
+      method: 'POST',
+      body: data,
+    });
+  },
+
+  async login(data: LoginInput) {
+    return api<AuthMessageResponse>('/auth/login', {
+      method: 'POST',
+      body: data,
+    });
+  },
+
+  async logout() {
+    return api<AuthMessageResponse>('/auth/logout', {
+      method: 'POST',
+    });
+  },
+
+  async refresh() {
+    return api<AuthMessageResponse>('/auth/refresh', {
+      method: 'POST',
+    });
+  },
+};
